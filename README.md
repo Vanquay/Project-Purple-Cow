fad219cf446fdd2b31d8505c0ebe2349caa3e8bb
Vanquay <vanquay16@gmail.com>
2022-10-14 15:55:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project Description
+Proof of Concept for a RESTFUL application that carries health supplies from Black Owned Businesses. 
+
+
+### Reference Documentation
+Please refer to the [solution.md](solution.md)
+
+
---
+++ src/main/java/com/project/purpleCow/PurpleCowApplication.java
@@ -0,0 +1,13 @@
+package com.project.purpleCow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpleCowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurpleCowApplication.class, args);
+	}
+
+}
